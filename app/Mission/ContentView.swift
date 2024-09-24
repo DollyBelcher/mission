03eae36e13e90e74e8acb0330a0388e1a1c5527d
Dolly Belcher <dollybelcher@hotmail.com>
2024-09-24 17:46:16 +0100

    Set up virtual machine to create app

---
+++ app/Mission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mission
+//
+//  Created by user257756 on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/Mission/MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionApp.swift
+//  Mission
+//
+//  Created by user257756 on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
